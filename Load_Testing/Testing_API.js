59bd067821ab0010c78953f87b489f921d17833f
WahyuElfan <elfanwahyu11@gmail.com>
2025-04-24 23:03:07 +0700

    add sla testing_api.js

--- Load_Testing/Testing_API.js
+++ Load_Testing/Testing_API.js
@@ -1,7 +1,5 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-
 
 export let options = {
   vus: 50, // Number of virtual users
