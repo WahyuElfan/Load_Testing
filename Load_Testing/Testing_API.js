cac2b0439f1d84502dc189049204ba727873e8ff
WahyuElfan <elfanwahyu11@gmail.com>
2025-04-24 23:04:55 +0700

    update comment script

--- Load_Testing/Testing_API.js
+++ Load_Testing/Testing_API.js
@@ -2,13 +2,19 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export let options = {
+  thresholds: {
+    http_req_duration: ['p(95)<500'], // SLA: 95% request done < 500ms
+  },
   vus: 50, // Number of virtual users
   duration: '30s', // Test duration
 };
 
 export default function () {
-  let res = http.get('https://reqres.in/api/users');
-  console.log()
+
+   // Send a GET request to the URL and add the query parameter 'page=1'
+  const res = http.get('https://reqres.in/api/users', {param : { page: 1}});
+
+  console.log('status code = ${res.status}')
 
   // Check if response status is 200
   check(res, {
